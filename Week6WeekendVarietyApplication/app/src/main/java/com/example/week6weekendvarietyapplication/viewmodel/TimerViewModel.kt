2eb3fa9ea37e7750baf6587673412b7568b423d0
Jose Catalasan <jose.catalasan@mobileappscompany.us>
2019-09-22 11:20:01 -0400

    added more granularity to timer counter

--- Week6WeekendVarietyApplication/app/src/main/java/com/example/week6weekendvarietyapplication/viewmodel/TimerViewModel.kt
+++ Week6WeekendVarietyApplication/app/src/main/java/com/example/week6weekendvarietyapplication/viewmodel/TimerViewModel.kt
@@ -10,22 +10,22 @@ class TimerViewModel : ViewModel() {
 
     private var timer : Timer
     val timerCount = MutableLiveData<String>()
-    val duration = MutableLiveData<Int>()
-    var counter = 0
+    val duration = MutableLiveData<Double>()
+    var counter = 0.0
 
     init{
         timer = Timer()
-        duration.value = 5
+        duration.value = 5.0
         timerCount.value = "Enter Timer Duration"
     }
 
     fun durationChange(editable : Editable){
         if(editable.toString() != "")
-            duration.postValue(editable.toString().toInt())
+            duration.postValue(editable.toString().toDouble())
     }
 
     fun reset(){
-        counter = 0
+        counter = 0.0
         timer.cancel()
     }
 
@@ -34,18 +34,19 @@ class TimerViewModel : ViewModel() {
         timer = Timer()
         timer.scheduleAtFixedRate(object : TimerTask(){
             override fun run() {
-                counter += 1
+                counter += 0.1
+                counter ="%.2f".format(counter).toDouble()
                 timerCount.postValue("In progress: $counter")
                 if(counter >= duration.value!!){
                     reset()
                     timerCount.postValue("Completed")
                 }
             }
-        }, 0,1000)
+        }, 0,100)
     }
 
     fun onStopClicked() {
-        if(counter > 0) {
+        if(counter > 0.0) {
             reset()
             timerCount.value = "Stopped"
         }
